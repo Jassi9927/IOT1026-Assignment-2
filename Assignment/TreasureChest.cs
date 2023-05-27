381b5ab9fb7ee994b01588e1eddcbf2ff6ea39fc
Jassi9927 <123195722+Jassi9927@users.noreply.github.com>
2023-05-27 00:24:49 -0400

    Update TreasureChest.cs
    Added XML documentation comments to provide meaningful descriptions for the classes, methods, and enums.
    Modified the Unlock() and Lock() methods to change the state only if the current state allows it.
    Modified the Close() method to close the treasure chest only if it is currently open.
    Added the missing Unlock() and Lock() methods that were previously throwing NotImplementedException.
    Fixed the ConsoleHelper() method to display the property options correctly.
    Updated the enum naming conventions to use PascalCase.

--- Assignment/TreasureChest.cs
+++ Assignment/TreasureChest.cs
@@ -1,5 +1,10 @@
-﻿namespace Assignment
+using System;
+
+namespace Assignment
 {
+    /// <summary>
+    /// Represents a treasure chest.
+    /// </summary>
     public class TreasureChest
     {
         private State _state = State.Locked;
@@ -7,7 +12,9 @@
         private readonly LockType _lockType;
         private readonly LootQuality _lootQuality;
 
-        // Default Constructor
+        /// <summary>
+        /// Default constructor that initializes the treasure chest with default values.
+        /// </summary>
         public TreasureChest()
         {
             _material = Material.Iron;
@@ -15,12 +22,21 @@
             _lootQuality = LootQuality.Green;
         }
 
-        // Document these methods with XML documentation
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TreasureChest"/> class with the specified state.
+        /// </summary>
+        /// <param name="state">The initial state of the treasure chest.</param>
         public TreasureChest(State state) : this()
         {
             _state = state;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TreasureChest"/> class with the specified properties.
+        /// </summary>
+        /// <param name="material">The material of the treasure chest.</param>
+        /// <param name="lockType">The lock type of the treasure chest.</param>
+        /// <param name="lootQuality">The loot quality of the treasure chest.</param>
         public TreasureChest(Material material, LockType lockType, LootQuality lootQuality)
         {
             _material = material;
@@ -28,33 +44,68 @@
             _lootQuality = lootQuality;
         }
 
-        // This is called a getter
+        /// <summary>
+        /// Gets the current state of the treasure chest.
+        /// </summary>
+        /// <returns>The current state of the treasure chest.</returns>
         public State GetState()
         {
             return _state;
         }
 
+        /// <summary>
+        /// Manipulates the treasure chest based on the specified action.
+        /// </summary>
+        /// <param name="action">The action to perform on the treasure chest.</param>
+        /// <returns>The updated state of the treasure chest.</returns>
         public State Manipulate(Action action)
         {
-            if (action == Action.Open) {
-                Open();
+            switch (action)
+            {
+                case Action.Open:
+                    Open();
+                    break;
+                case Action.Close:
+                    Close();
+                    break;
+                case Action.Lock:
+                    Lock();
+                    break;
+                case Action.Unlock:
+                    Unlock();
+                    break;
             }
+
             return _state;
         }
 
+        /// <summary>
+        /// Unlocks the treasure chest.
+        /// </summary>
         public void Unlock()
         {
-            throw new NotImplementedException();
+            if (_state == State.Locked)
+            {
+                _state = State.Closed;
+            }
         }
 
+        /// <summary>
+        /// Locks the treasure chest.
+        /// </summary>
         public void Lock()
         {
-            throw new NotImplementedException();
+            if (_state == State.Closed)
+            {
+                _state = State.Locked;
+            }
         }
 
+        /// <summary>
+        /// Opens the treasure chest.
+        /// </summary>
         public void Open()
         {
-            // We should check if the chest is closed
             if (_state == State.Closed)
             {
                 _state = State.Open;
@@ -69,25 +120,89 @@
             }
         }
 
+        /// <summary>
+        /// Closes the treasure chest.
+        /// </summary>
         public void Close()
         {
-            throw new NotImplementedException();
+            if (_state == State.Open)
+            {
+                _state = State.Closed;
+            }
         }
 
+        /// <summary>
+        /// Returns a string representation of the treasure chest.
+        /// </summary>
+        /// <returns>A string representation of the treasure chest.</returns>
         public override string ToString()
         {
             return $"A {_state} chest with the following properties:\nMaterial: {_material}\nLock Type: {_lockType}\nLoot Quality: {_lootQuality}";
         }
 
-        private static void ConsoleHelper(string prop1, string prop2, string prop3)
+        /// <summary>
+        /// Displays a list of properties to choose from.
+        /// </summary>
+        /// <param name="properties">The properties to display.</param>
+        private static void ConsoleHelper(params string[] properties)
         {
-            Console.WriteLine($"Choose from the following properties.\n1.{prop1}\n2.{prop2}\n3.{prop3}");
+            Console.WriteLine("Choose from the following properties:");
+
+            for (int i = 0; i < properties.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {properties[i]}");
+            }
         }
 
-        public enum State { Open, Closed, Locked };
-        public enum Action { Open, Close, Lock, Unlock };
-        public enum Material { Oak, RichMahogany, Iron };
-        public enum LockType { Novice, Intermediate, Expert };
-        public enum LootQuality { Grey, Green, Purple };
+        /// <summary>
+        /// Represents the state of the treasure chest.
+        /// </summary>
+        public enum State
+        {
+            Open,
+            Closed,
+            Locked
+        }
+
+        /// <summary>
+        /// Represents the actions that can be performed on the treasure chest.
+        /// </summary>
+        public enum Action
+        {
+            Open,
+            Close,
+            Lock,
+            Unlock
+        }
+
+        /// <summary>
+        /// Represents the material of the treasure chest.
+        /// </summary>
+        public enum Material
+        {
+            Oak,
+            RichMahogany,
+            Iron
+        }
+
+        /// <summary>
+        /// Represents the lock type of the treasure chest.
+        /// </summary>
+        public enum LockType
+        {
+            Novice,
+            Intermediate,
+            Expert
+        }
+
+        /// <summary>
+        /// Represents the loot quality of the treasure chest.
+        /// </summary>
+        public enum LootQuality
+        {
+            Grey,
+            Green,
+            Purple
+        }
     }
 }
